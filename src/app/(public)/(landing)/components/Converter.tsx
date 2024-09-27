2bf467b36978a3adb909f9074a4dcdf7b95812b6
Jean Robertou <j-rob@live.fr>
2024-09-27 19:20:07 +0200

    fix: fix width on mobile

--- src/app/(public)/(landing)/components/Converter.tsx
+++ src/app/(public)/(landing)/components/Converter.tsx
@@ -5,7 +5,7 @@ export const Converter = () => {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 md:px-8 lg:px-16">
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-5">
-        <div className="w-2/3 rounded-xl border-[1px] border-slate-300 bg-white p-8 shadow-xl lg:col-span-2">
+        <div className="w-full rounded-xl border-[1px] border-slate-300 bg-white p-8 shadow-xl lg:col-span-2 lg:w-2/3">
           <InputColour />
         </div>
 
