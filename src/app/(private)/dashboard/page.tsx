a91c880a3f3f0eaf65ad6615a1bcaa8fb7107407
Jean Robertou <j-rob@live.fr>
2024-09-27 17:55:59 +0200

    chore: delete useless pages and routes

--- src/app/(private)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useSessionContext } from '@/providers/sessionContext'
-
-export default async function DashboardPage() {
-  const { user } = useSessionContext()
-
-  return <>DASHBOARD</>
-}
